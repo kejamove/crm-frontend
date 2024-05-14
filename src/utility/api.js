f76927433cf0ed1571dee0ce71c02be86d751d0f
muasya-victor <vicmwe184@gmail.com>
2024-05-14 09:59:49 +0300

    update backend url

--- src/utility/api.js
+++ src/utility/api.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 // import { baseUrl } from "constants";
-export const baseUrl = "http://localhost:8000/api/"
+// export const baseUrl = "http://localhost:8000/api/"
+export const baseUrl = "https://72db-102-0-4-206.ngrok-free.app/api/"
 
 
 export default axios.create({
