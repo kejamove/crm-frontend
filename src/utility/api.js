0c97496e9535fc15ff3ff09bf6ac27307eb822e3
muasya-victor <vicmwe184@gmail.com>
2024-05-17 09:44:23 +0300

    update url

--- src/utility/api.js
+++ src/utility/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 // import { baseUrl } from "constants";
 // export const baseUrl = "http://localhost:8000/api/"
-export const baseUrl = "https://91ad-105-163-42-119.ngrok-free.app/api/"
+export const baseUrl = "https://test.kejadigital.com/api/"
 
 
 export default axios.create({
