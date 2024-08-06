7e55f4f3089aaddf5ac778f873f50baf2a308780
victor muasya <vicmwe184@gmail.com>
2024-08-06 15:13:35 +0300

    add chartjs

--- src/utility/constants.js
+++ src/utility/constants.js
@@ -3,7 +3,7 @@ export const baseUrl = "https://kejacrm-api-0d785412ff3f.herokuapp.com/api/"
 
 export const headers = {
     "Content-Type": "application/json",
-    "credentials": "false"
+    "credentials": true
 };
 export const userType = JSON.parse(localStorage.getItem("authData"))?.user?.user_type;
 export const activeUser = JSON.parse(localStorage.getItem("authData"))?.user;
