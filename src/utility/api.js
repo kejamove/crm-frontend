df24a42bf43bfd22a8526a20fefcf9d661548673
victor muasya <vicmwe184@gmail.com>
2024-08-10 03:09:07 +0300

    update backend url

--- src/utility/api.js
+++ src/utility/api.js
@@ -2,8 +2,9 @@ import axios from "axios"
 // import { baseUrl } from "constants";
 // export const baseUrl = "https://16.16.127.250/api/"
 // export const baseUrl = "http://localhost:8000/api/"
+export const baseUrl = "https://crm-node-api-38131eb86996.herokuapp.com/api/"
 // export const baseUrl = "https://test.kejadigital.com/api/"
-export const baseUrl = "https://kejacrm-api-0d785412ff3f.herokuapp.com/api/"
+// export const baseUrl = "https://kejacrm-api-0d785412ff3f.herokuapp.com/api/"
 // export const baseUrl = "https://secure-brushlands-73777-bf5e5d7d211f.herokuapp.com/api/"
 
 
