ca93a3fb77b3fa2c15bc7e00456c41a80bcc2d61
victor muasya <vicmwe184@gmail.com>
2024-10-08 20:52:29 +0300

    correct get user role

--- src/utility/constants.js
+++ src/utility/constants.js
@@ -3,7 +3,7 @@ export const baseUrl = "https://kejacrm-api-0d785412ff3f.herokuapp.com/api/"
 export const headers = {
     "Content-Type": "application/json",
 };
-export const userType = JSON.parse(localStorage.getItem("authData"))?.user?.user_type;
+export const userType = JSON.parse(localStorage.getItem("authData"))?.user?.role;
 export const activeUser = JSON.parse(localStorage.getItem("authData"))?.user;
 
 const projectName = 'KEJA FRONTEND'
